455da77ebf6dfaeb59d14ffccd7fc9886afa0c3b
saurabhj80 <saurabhj80@gmail.com>
2015-05-08 14:00:11 -0700

    Minro

--- Learning Core Graphics/View/SJView.swift
+++ Learning Core Graphics/View/SJView.swift
@@ -191,20 +191,10 @@ class SJLine: SJBaseView {
 *******************************/
 class SJRatingView: SJBaseView {
     
-    private var numberOfStars = 5 {
+    var numberOfStars = 5 {
         didSet{
             setNeedsDisplay()
         }
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setUp()
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setUp()
     }
     
     private override func setUp() {
